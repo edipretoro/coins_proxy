629ca2055c3f76088498bcd83f6ee6262e1d49de
Emmanuel Di Pretoro <edipretoro@gmail.com>
2009-04-04 00:23:50 +0200

    Add a empty filter to my proxy

--- coins_proxy.pl
+++ coins_proxy.pl
@@ -4,10 +4,22 @@ use strict;
 use warnings;
 
 use HTTP::Proxy;
+use HTTP::Proxy::BodyFilter::complete;
+use HTTP::Proxy::BodyFilter::simple;
 
 my $proxy = HTTP::Proxy->new( @ARGV );
 
+$proxy->push_filter(
+    mime => 'text/html',
+    response => HTTP::Proxy::BodyFilter::complete->new(),
+    response => HTTP::Proxy::BodyFilter::simple->new(
+        sub {
+            my ( $self, $dataref, $message, $protocol, $buffer ) = @_;
 
+            
+        }        
+    ),
+);
 
 $proxy->start;
 
