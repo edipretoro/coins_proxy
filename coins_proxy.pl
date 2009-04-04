73c2d0bb3d8265b2c878bfbb505509bdff63230c
Emmanuel Di Pretoro <edipretoro@gmail.com>
2009-04-04 22:29:07 +0200

    Add the processing of the COinS

--- coins_proxy.pl
+++ coins_proxy.pl
@@ -10,6 +10,9 @@ use HTTP::Proxy::BodyFilter::simple;
 use HTML::TreeBuilder::XPath;
 use Data::Dump qw( dump );
 
+use URI;
+use URI::QueryParam;
+
 my $proxy = HTTP::Proxy->new( @ARGV );
 
 $proxy->push_filter(
@@ -20,10 +23,26 @@ $proxy->push_filter(
             my ( $self, $dataref, $message, $protocol, $buffer ) = @_;
 
             my $tree = HTML::TreeBuilder::XPath->new_from_content( $$dataref );
-            my $coins_xpath = '//span[@class="Z3988"]/@title';
+            my $coins_xpath = '//span[@class="Z3988"]';
 
-            my $coins = $tree->findvalue( $coins_xpath );
-            print "The COinS is " . $coins . "\n";
+            my $coins = $tree->findnodes( $coins_xpath );
+
+            my $coins_title_xpath = './@title';
+            foreach (@$coins) {
+                my $coins_title = $_->findvalue( $coins_title_xpath );
+                
+                # This part will be updated when I'll find a OpenURL resolver
+                my $uri = URI->new( 'http://www.bib.ulb.ac.be' . '?' . $coins_title );
+                my $params = $uri->query_form_hash();
+                my $issn = $params->{'rft.issn'};
+                my $link_uri = URI->new( 'http://bib7.ulb.ac.be/uhtbin/ISSN/' . $issn);
+                my $link = HTML::Element->new( 'a', href => $link_uri->as_string );
+                $link->push_content('ZOZO');
+                $_->push_content( $link );
+                # End of this part
+            }
+
+            $$dataref = $tree->as_HTML;
         }        
     ),
 );
